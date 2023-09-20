b823a3ec34f179f9586325972cff62068a4133b8
joonaskiuru <joonas.kiuru@gmail.com>
2023-09-20 18:13:12 +0300

    feat: color changer button

--- script.js
+++ script.js
@@ -26,3 +26,18 @@ function main() {
   window.addEventListener("popstate", updateActive);
   updateActive(); // Also update immediately.
 }
+
+// Color Generator
+function generateNewColor() {
+  btn = document.getElementById('color-changer');
+  let color = "#";
+  let x = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D', 'E','F']
+
+  for(let i = 0; i < 6;i++){
+      const random = Math.floor(Math.random() * x.length);
+      color += x[random];
+  }
+
+  btn.style.background = color;
+  return;
+}
