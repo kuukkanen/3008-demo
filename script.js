57dbb549ed490c8017e8c27f5a51ccdbd1a3f085
Jarkko Kuukkanen <jarkko.kuukkanen@tuni.fi>
2023-09-20 14:00:57 +0300

    refactor: improve location handling

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 function main() {
   // Get all "buttons" from the header.
   const buttons = document.querySelectorAll(".header nav a");
+  // Get the default hash from the first button.
+  const defaultHash = new URL(buttons[0].href).hash;
 
   function updateActive() {
+    let hasActive = false;
     // For each button in the header.
     for (const button of buttons) {
       // Get the hash part from the button link's URL.
@@ -12,14 +15,14 @@ function main() {
       const currentHash = document.location.hash;
       // Make it active if it's the current one.
       button.classList.toggle("active", hash === currentHash);
+      // If active is found set `hasActive` to true.
+      if (hash === currentHash) hasActive = true;
     }
+    // If no button was activated. This means that the demo is invalid.
+    if (!hasActive) location.replace(defaultHash); // Use the default hash.
   }
 
   // Listen to history changes.
   window.addEventListener("popstate", updateActive);
-
-  // Make "#home" demo active by default.
-  const currentLocation = document.location;
-  currentLocation.hash = "#home";
-  location.replace(currentLocation);
+  updateActive(); // Also update immediately.
 }
