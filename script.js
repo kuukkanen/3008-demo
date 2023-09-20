bce04c83337278828497ce1ac6e52b1b8cf81efb
Jarkko Kuukkanen <jarkko.kuukkanen@tuni.fi>
2023-09-20 12:48:50 +0300

    refactor: format with Prettier

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ console.log(btns);
 
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
+  btns[i].addEventListener("click", function () {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
