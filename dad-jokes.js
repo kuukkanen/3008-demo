83f17ae7dad2758e0773653412558025db4175d8
Lassi Inkinen <lassi.inkinen@tuni.fi>
2023-09-27 21:53:14 +0300

    feat: dad-jokes.js container and header txt for demo

--- dad-jokes.js
+++ dad-jokes.js
@@ -1,4 +1,12 @@
 (function () {
+const div = document.createElement("div");
+div.style.width = "450px";
+div.style.padding = "50px 20px";
+div.style.backgroundColor = "#FF0FF4";
 
+const h1 = document.createElement("H1");
+h1.innerHTML = "Dad Jokes";
 
+content.appendChild(div)
+div.appendChild(h1)
 })();
