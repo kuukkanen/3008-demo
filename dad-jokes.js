24b04c27211b138a312ee13cbe02e43135218c77
Lassi Inkinen <lassi.inkinen@tuni.fi>
2023-09-28 22:54:56 +0300

    add: <p> to display joke text for dadjokes

--- dad-jokes.js
+++ dad-jokes.js
@@ -7,11 +7,15 @@ div.style.backgroundColor = "#FF0FF4";
 const button = document.createElement("button");
 button.textContent = "New Joke";
 
-const h1 = document.createElement("H1");
+const h1 = document.createElement("h1");
 h1.innerHTML = "Dad Jokes";
+
+const jokeText = document.createElement("p");
+jokeText.innerHTML = "What do you call a magician who has lost their magic? Ian."
 
 content.appendChild(div)
 div.appendChild(h1)
+div.appendChild(jokeText)
 div.appendChild(button)
 
 })();
