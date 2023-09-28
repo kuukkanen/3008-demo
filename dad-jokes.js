dd2c801962089552bdf5be681f99da6b3bd75025
Lassi Inkinen <lassi.inkinen@tuni.fi>
2023-09-28 22:40:20 +0300

    add: button for dadjokes demo

--- dad-jokes.js
+++ dad-jokes.js
@@ -4,9 +4,14 @@ div.style.width = "450px";
 div.style.padding = "50px 20px";
 div.style.backgroundColor = "#FF0FF4";
 
+const button = document.createElement("button");
+button.textContent = "New Joke";
+
 const h1 = document.createElement("H1");
 h1.innerHTML = "Dad Jokes";
 
 content.appendChild(div)
 div.appendChild(h1)
+div.appendChild(button)
+
 })();
