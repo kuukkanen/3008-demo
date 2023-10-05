2490be2d34547e92a35d99a39f1ee7aa250405ba
Jarkko Kuukkanen <jarkko.kuukkanen@tuni.fi>
2023-10-05 12:47:02 +0300

    add: game canvas

---
+++ breakout.js
@@ -0,0 +1,15 @@
+(function () {
+  // Create canvas for the game.
+  const canvas = document.createElement("canvas");
+
+  // Reasonable size.
+  canvas.width = 640;
+  canvas.height = 480;
+
+  // Get the drawing context.
+  const ctx = canvas.getContext("2d", { alpha: false });
+
+  content.appendChild(canvas); // eslint-disable-line
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+})();
