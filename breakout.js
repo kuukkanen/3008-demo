a12bbcaa055b2ae5a4bbfd9129d790221a25599a
Jarkko Kuukkanen <jarkko.kuukkanen@tuni.fi>
2023-10-05 12:53:21 +0300

    add: canvas deletion observer

--- breakout.js
+++ breakout.js
@@ -12,4 +12,15 @@
   content.appendChild(canvas); // eslint-disable-line
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Create observer for the container element.
+  const observer = new MutationObserver((e) => {
+    // The canvas was removed.
+    if (e[0].removedNodes) {
+      observer.disconnect(); // Stop observing.
+    }
+  });
+
+  // Observer DOM changes.
+  observer.observe(content, { childList: true }); // eslint-disable-line
 })();
