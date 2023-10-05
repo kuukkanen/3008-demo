e63e8955a21aa5b302a6c9b996ab8c53fb2f21a5
Jarkko Kuukkanen <jarkko.kuukkanen@tuni.fi>
2023-10-05 12:55:58 +0300

    add: basic gameloop

--- breakout.js
+++ breakout.js
@@ -11,16 +11,26 @@
 
   content.appendChild(canvas); // eslint-disable-line
 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // True while the game is running.
+  let isRunning = true;
 
   // Create observer for the container element.
   const observer = new MutationObserver((e) => {
     // The canvas was removed.
     if (e[0].removedNodes) {
       observer.disconnect(); // Stop observing.
+      isRunning = false;
     }
   });
 
   // Observer DOM changes.
   observer.observe(content, { childList: true }); // eslint-disable-line
+
+  const draw = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Next frame while the game is running.
+    if (isRunning) window.requestAnimationFrame(draw);
+  };
+  draw();
 })();
