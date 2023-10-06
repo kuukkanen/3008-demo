f5874268fc091fb12d74888342024a99b73383a1
joonaskiuru <joonas.kiuru@gmail.com>
2023-10-06 10:30:47 +0300

    refactor: prettier

--- stopwatch.js
+++ stopwatch.js
@@ -1,5 +1,4 @@
 (function () {
-
   let element = `<div class="stopwatch">
                 <h1>Stopwatch</h1>
                 <div id="stopwatch">00:00.00</div>
@@ -8,7 +7,7 @@
                 </div> `;
   content = document.getElementById("content"); // eslint-disable-line
   content.innerHTML = element; // eslint-disable-line
-  
+
   let startTime;
   let intervalId;
   let running = false;
@@ -21,42 +20,48 @@
   let totalTime = 0;
 
   startStopButton.addEventListener("click", () => {
-      if (running) {
-          stop();
-      } else {
-          start();
-      }
+    if (running) {
+      stop();
+    } else {
+      start();
+    }
   });
 
   resetButton.addEventListener("click", reset);
 
   function start() {
-      running = true;
-      startStopButton.textContent = "Stop";
-      previousTime = Date.now();
-      intervalId = setInterval(updateDisplay, 100);
+    // Start the clock
+    running = true;
+    startStopButton.textContent = "Stop";
+    previousTime = Date.now();
+    intervalId = setInterval(updateDisplay, 100);
   }
 
   function stop() {
-      running = false;
-      startStopButton.textContent = "Start";
-      clearInterval(intervalId);
+    //
+    running = false;
+    startStopButton.textContent = "Start";
+    clearInterval(intervalId);
   }
 
   function reset() {
-      stop();
-      totalTime = 0;
-      stopwatchDisplay.textContent = "00:00:00";
+    stop();
+    totalTime = 0;
+    stopwatchDisplay.textContent = "00:00:00";
   }
 
   function updateDisplay() {
+    // Current time  = approx 100 ms.
+    const interval = Date.now() - previousTime;
+    previousTime = Date.now();
+    totalTime += interval;
 
-    // Current time  = n. 100 ms.
-      const interval = Date.now() - previousTime;
-      previousTime = Date.now();
-      totalTime += interval;
-
-      const displayTime = `${String(Math.floor(totalTime / 60000)).padStart(2, "0")}:${String(Math.floor((totalTime / 1000) % 60)).padStart(2, "0")}.${String(Math.floor(totalTime % 1000 / 10)).padStart(2, "0")}`;
-      stopwatchDisplay.textContent = displayTime;
+    const displayTime = `${String(Math.floor(totalTime / 60000)).padStart(
+      2,
+      "0"
+    )}:${String(Math.floor((totalTime / 1000) % 60)).padStart(2, "0")}.${String(
+      Math.floor((totalTime % 1000) / 10)
+    ).padStart(2, "0")}`;
+    stopwatchDisplay.textContent = displayTime;
   }
 })();
