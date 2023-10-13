3ae6e109c5d1d2f611b77b8fab8e4245e803b3e8
joonaskiuru <joonas.kiuru@gmail.com>
2023-10-13 17:52:16 +0300

    add: video #2

--- videos.js
+++ videos.js
@@ -1,5 +1,17 @@
 (function () {
-  let element = `<iframe width="560" height="315" src="https://www.youtube.com/embed/dWqNgzZwVJQ?si=jAjCQI34pe47FcQH" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> `;
+  let element = `<div id="video_container"><h1>Videos</h1></div>`;
   content = document.getElementById("content"); // eslint-disable-line
   content.innerHTML = element; // eslint-disable-line
+  let videos = [
+    "https://www.youtube.com/embed/dWqNgzZwVJQ?si=jAjCQI34pe47FcQH",
+    "https://www.youtube.com/embed/5ChkQKUzDCs?si=GJdf4fDM66FkKiMe",
+  ];
+
+  for (let i = 0; i < videos.length; i++) {
+    let video_link = `<iframe width="560" height="315" src=${videos[i]} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> `;
+    let video_element = document.createElement("div");
+    video_element.innerHTML = video_link;
+    content.appendChild(video_element);
+    console.log(videos[i]);
+  }
 })();
