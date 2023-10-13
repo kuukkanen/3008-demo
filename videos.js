f620d5d6845c53ecb7e17e651de439ad8ff3681d
joonaskiuru <joonas.kiuru@gmail.com>
2023-10-13 18:04:31 +0300

    add: video #3

--- videos.js
+++ videos.js
@@ -4,12 +4,13 @@
   content = document.getElementById("content"); // eslint-disable-line
   content.innerHTML = element; // eslint-disable-line
   let videos = [
-    "https://www.youtube.com/embed/dWqNgzZwVJQ?si=jAjCQI34pe47FcQH",
-    "https://www.youtube.com/embed/5ChkQKUzDCs?si=GJdf4fDM66FkKiMe",
+    '<iframe width="560" height="315" src="https://www.youtube.com/embed/dWqNgzZwVJQ?si=ID65iTA82MTM6FMW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+    '<iframe width="560" height="315" src="https://www.youtube.com/embed/5ChkQKUzDCs?si=fR3klGmI_JGXqH_F" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+    '<iframe width="560" height="315" src="https://www.youtube.com/embed/m4-HM_sCvtQ?si=Xdq4fItwVOnSAANR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
   ];
 
   for (let i = 0; i < videos.length; i++) {
-    let video_link = `<iframe width="560" height="315" src=${videos[i]} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> `;
+    let video_link = videos[i];
     let video_element = document.createElement("div");
     video_element.classList.add("video");
     video_element.innerHTML = video_link;
