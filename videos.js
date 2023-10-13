cda3d5af90d9ebb06d28a27d1ef5cffeaa8ea2ca
joonaskiuru <joonas.kiuru@gmail.com>
2023-10-13 17:34:58 +0300

    add: video #1

---
+++ videos.js
@@ -0,0 +1,5 @@
+(function () {
+  let element = `<iframe width="560" height="315" src="https://www.youtube.com/embed/dWqNgzZwVJQ?si=jAjCQI34pe47FcQH" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> `;
+  content = document.getElementById("content"); // eslint-disable-line
+  content.innerHTML = element; // eslint-disable-line
+})();
