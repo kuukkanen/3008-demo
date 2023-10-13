9b4fbc75081e79b239e05ba77fa31ba239131387
joonaskiuru <joonas.kiuru@gmail.com>
2023-10-13 18:06:26 +0300

    refactor: comments

--- videos.js
+++ videos.js
@@ -1,8 +1,11 @@
 (function () {
+  // Header element with divider
   let element = `<div><h1>Videos</h1></div>
                  <hr class="solid">`;
   content = document.getElementById("content"); // eslint-disable-line
   content.innerHTML = element; // eslint-disable-line
+
+  // All videos in an array
   let videos = [
     '<iframe width="560" height="315" src="https://www.youtube.com/embed/dWqNgzZwVJQ?si=ID65iTA82MTM6FMW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
     '<iframe width="560" height="315" src="https://www.youtube.com/embed/5ChkQKUzDCs?si=fR3klGmI_JGXqH_F" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
@@ -10,6 +13,7 @@
     '<iframe width="560" height="315" src="https://www.youtube.com/embed/pEfrdAtAmqk?si=cSfRxl5eqQweceKo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
   ];
 
+  // Loop to add all videos to site
   for (let i = 0; i < videos.length; i++) {
     let video_link = videos[i];
     let video_element = document.createElement("div");
