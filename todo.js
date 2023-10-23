caeefb8789b8e7c97228534ac9897b6961ae2e50
Lassi Inkinen <lassi.inkinen@tuni.fi>
2023-10-23 13:04:17 +0300

    add: html input text field

--- todo.js
+++ todo.js
@@ -2,6 +2,7 @@
   let element = `
     <div class="container">
       <h1>ToDo List</h1>
+      <input type="text" id="myInput" placeholder="Title..." />
     </div>`;
   content = document.getElementById("content"); // eslint-disable-line
   content.innerHTML = element; // eslint-disable-line
