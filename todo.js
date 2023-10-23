2e7a77b4850bb174e20349611cd6901594b39785
Lassi Inkinen <lassi.inkinen@tuni.fi>
2023-10-23 12:48:59 +0300

    add: html container and heading

--- todo.js
+++ todo.js
@@ -1,1 +1,8 @@
-(function () {})();
+(function () {
+  let element = `
+    <div class="container">
+      <h1>ToDo List</h1>
+    </div>`;
+  content = document.getElementById("content"); // eslint-disable-line
+  content.innerHTML = element; // eslint-disable-line
+})();
