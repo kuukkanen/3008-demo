2d991363b3b92e27a24980e4c40c4eb66d21a954
Lassi Inkinen <lassi.inkinen@tuni.fi>
2023-10-23 13:06:52 +0300

    add: html example list element

--- todo.js
+++ todo.js
@@ -3,6 +3,10 @@
     <div class="container">
       <h1>ToDo List</h1>
       <input type="text" id="myInput" placeholder="Title..." />
+      <ul id="myUL">
+        <li>Get groceries</li>
+        <li>Pay the bills</li>
+      </ul>
     </div>`;
   content = document.getElementById("content"); // eslint-disable-line
   content.innerHTML = element; // eslint-disable-line
