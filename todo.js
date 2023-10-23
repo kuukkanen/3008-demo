f7737de7df52f2b03772b5d2cd128470fa0143f5
Lassi Inkinen <lassi.inkinen@tuni.fi>
2023-10-23 13:21:15 +0300

    feat: add your own list elements

--- todo.js
+++ todo.js
@@ -3,6 +3,10 @@
     <div class="container">
       <h1>ToDo List</h1>
       <input type="text" id="myInput" placeholder="Title..." />
+      <span id="addElement">
+          Add
+      </span>
+
       <ul id="myUL">
         <li>Get groceries</li>
         <li>Pay the bills</li>
@@ -10,4 +14,12 @@
     </div>`;
   content = document.getElementById("content"); // eslint-disable-line
   content.innerHTML = element; // eslint-disable-line
+
+  document.getElementById("addElement").addEventListener("click", function () {
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("myInput").value;
+    let t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    document.getElementById("myUL").appendChild(li);
+  });
 })();
