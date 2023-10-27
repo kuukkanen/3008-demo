874c0ce0e8a14cae7d90161ef1af87ceb3009ab8
Lassi Inkinen <lassi.inkinen@tuni.fi>
2023-10-28 00:52:08 +0300

    refactor: variable declaration

--- todo.js
+++ todo.js
@@ -1,5 +1,5 @@
 (function () {
-  let element = `
+  const element = `
     <div class="container">
       <h1>ToDo List</h1>
       <div class="header">
@@ -18,7 +18,7 @@
   content.innerHTML = element; // eslint-disable-line
 
   // toggle "line-through" when clicking on a list item
-  let list = document.querySelector("#myUL");
+  const list = document.querySelector("#myUL");
   list.addEventListener(
     "click",
     (ev) => {
@@ -30,29 +30,29 @@
   );
 
   // Create a "close" button and append it to each list item
-  let myNodelist = document.getElementById("myUL").getElementsByTagName("LI"); //only myUL li:s
+  const myNodelist = document.getElementById("myUL").getElementsByTagName("LI"); //only myUL li:s
   for (i = 0; i < myNodelist.length; i++) {
-    let span = document.createElement("SPAN");
-    let txt = document.createTextNode("X");
+    const span = document.createElement("SPAN");
+    const txt = document.createTextNode("X");
     span.className = "close";
     span.appendChild(txt);
     myNodelist[i].appendChild(span);
   }
 
   // Click on a close button to hide the current list item
-  var close = document.getElementsByClassName("close");
+  const close = document.getElementsByClassName("close");
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function () {
-      var div = this.parentElement;
+      const div = this.parentElement;
       div.style.display = "none";
     };
   }
 
   // click add to create new list item
   document.getElementById("addElement").addEventListener("click", function () {
-    let li = document.createElement("li");
-    let inputValue = document.getElementById("myInput").value;
-    let t = document.createTextNode(inputValue);
+    const li = document.createElement("li");
+    const inputValue = document.getElementById("myInput").value;
+    const t = document.createTextNode(inputValue);
     li.appendChild(t);
     if (inputValue === "") {
       alert("You must write something!");
@@ -62,15 +62,15 @@
     // clear input text
     document.getElementById("myInput").value = "";
 
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("X");
+    const span = document.createElement("SPAN");
+    const txt = document.createTextNode("X");
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
 
     for (i = 0; i < close.length; i++) {
       close[i].onclick = function () {
-        var div = this.parentElement;
+        const div = this.parentElement;
         div.style.display = "none";
       };
     }
