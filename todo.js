d8bab5b9ed42b41fde03868c242572fc6f46e0c7
Lassi Inkinen <lassi.inkinen@tuni.fi>
2023-10-28 00:46:05 +0300

    add: close onclick hide for li created by Add

--- todo.js
+++ todo.js
@@ -67,5 +67,12 @@
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+      };
+    }
   });
 })();
