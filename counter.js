dcf5b2e7d7ea06a3a618d7f4bf8659cdc95bf8c1
joonaskiuru <joonas.kiuru@gmail.com>
2023-10-29 20:30:31 +0200

    refactor: comments

--- counter.js
+++ counter.js
@@ -9,27 +9,32 @@
                     </div>`;
     content.innerHTML = element; // eslint-disable-line
 
+    // Get the HTML elements by id
     const increaseButton = document.getElementById("increaseButton");
     const decreaseButton = document.getElementById("decreaseButton");
     const resetButton = document.getElementById("resetButton");
     const counter = document.getElementById("counter");
     var digit = 0;
 
+    // The Increase Button increases digit by 1 when clicked
     increaseButton.addEventListener("click",() => {
         digit += 1;
         updateDisplay();
     });
 
+    // The Deccrease Button deccreases digit by 1 when clicked
     decreaseButton.addEventListener("click",() => {
         digit -= 1;
         updateDisplay();
     });
 
+    // The Reset Button resets digit to 0 when clicked
     resetButton.addEventListener("click",() => {
         digit = 0;
         updateDisplay();
     });
 
+    // Updates display to show current digit
     function updateDisplay (){
         counter.textContent = digit
     };
