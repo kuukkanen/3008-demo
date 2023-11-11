ae0cf71fb22773b6e650223a86e205766c263217
joonaskiuru <joonas.kiuru@gmail.com>
2023-11-11 13:48:24 +0200

    add: header to Fireship Videos

--- videos.js
+++ videos.js
@@ -3,11 +3,12 @@
 
   // Header element with divider
   const element = `<div><h1>Videos</h1></div>
-                 <hr class="solid">`;
+                 <hr class="solid">
+                 <div><h2>Fireship</h2></div>`;
   content.innerHTML = element; // eslint-disable-line
 
   // All videos in an array
-  const videos = [
+  const fireshipVideos = [
     '<iframe width="560" height="315" src="https://www.youtube.com/embed/dWqNgzZwVJQ?si=ID65iTA82MTM6FMW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
     '<iframe width="560" height="315" src="https://www.youtube.com/embed/5ChkQKUzDCs?si=fR3klGmI_JGXqH_F" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
     '<iframe width="560" height="315" src="https://www.youtube.com/embed/m4-HM_sCvtQ?si=Xdq4fItwVOnSAANR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
@@ -19,13 +20,12 @@
   ];
 
   // Loop to add all videos to site
-  for (let i = 0; i < videos.length; i++) {
-    const video_link = videos[i];
+  for (let i = 0; i < fireshipVideos.length; i++) {
+    const video_link = fireshipVideos[i];
     const video_element = document.createElement("div");
     video_element.classList.add("video");
     video_element.innerHTML = video_link;
     content.appendChild(video_element); // eslint-disable-line
-    console.log(videos[i]);
   }
 
   const divider = document.createElement("hr");
