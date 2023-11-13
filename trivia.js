0221753c1017c9e6049dfe278f3c9c30121748fb
Jarkko Kuukkanen <jarkko.kuukkanen@tuni.fi>
2023-11-13 20:11:42 +0200

    add: answer buttons

--- trivia.js
+++ trivia.js
@@ -15,7 +15,22 @@
     const question = document.createElement("p");
     // Use innerHTML as there might be HTML entities in the question text.
     question.innerHTML = `Question ${i + 1}. ${result.question}`;
+    question.style.fontSize = "1.5rem";
+    question.style.fontWeight = "bold";
 
     container.appendChild(question);
+
+    // Create a container for the buttons.
+    const buttonContainer = container.appendChild(
+      document.createElement("div"),
+    );
+
+    [result.correct_answer, ...result.incorrect_answers].forEach((answer) => {
+      // Create button for an (in)correct answer.
+      const button = buttonContainer.appendChild(
+        document.createElement("button"),
+      );
+      button.innerHTML = answer;
+    });
   });
 })();
