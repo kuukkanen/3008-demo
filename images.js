1564e9bc454c50b2318f5fa1d6df7d4cb873b060
joonaskiuru <joonas.kiuru@gmail.com>
2023-11-23 16:18:33 +0200

    add: Guitar #1

--- images.js
+++ images.js
@@ -42,6 +42,8 @@
         
         <hr class="solid">
         <h2>Guitars</h2>
+        <img class="medium-image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Guitar_1.jpg/640px-Guitar_1.jpg">    
+        
         
         
         <hr class="solid">`;
