39e345b75ee375b39676d6270e34914e3f36317b
joonaskiuru <joonas.kiuru@gmail.com>
2023-11-25 23:00:42 +0200

    add: container

--- dogs.js
+++ dogs.js
@@ -1,5 +1,9 @@
 (function () { 
     madeby.textContent = "Joonas Kiuru"; // eslint-disable-line
 
+      // Header element with divider
+  const element = `<div class="container">
+                    </div>`;
+content.innerHTML = element; // eslint-disable-line
 
 })();
