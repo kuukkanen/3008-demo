9a98a20604df3f4594933f9f2c29960a4f14d737
joonaskiuru <joonas.kiuru@gmail.com>
2023-11-25 23:06:52 +0200

    add: button

--- dogs.js
+++ dogs.js
@@ -4,7 +4,10 @@
       // Header element with divider
   const element = `<div class="container">
                     <h1>Dog Image Generator</h1>
+                    <button onClick=getDog()> Get Dog Image</button>
                     </div>`;
 content.innerHTML = element; // eslint-disable-line
+// https://dog.ceo/api/breeds/image/random
+
 
 })();
