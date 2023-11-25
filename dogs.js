1745611cc8be7d24a7353da2a955815fcb63e1bb
joonaskiuru <joonas.kiuru@gmail.com>
2023-11-26 00:15:55 +0200

    add: class medium-image to img

--- dogs.js
+++ dogs.js
@@ -4,7 +4,7 @@
       // Header element with divider
   const element = `<div class="container">
                     <h1>Dog Image Generator</h1>
-                    <img id="Dog" src="">
+                    <img id="Dog" class="medium-image" src="">
                     <button id="imageButton" class="medium-image"> Get Dog Image</button>
                     </div>`;
 content.innerHTML = element; // eslint-disable-line
