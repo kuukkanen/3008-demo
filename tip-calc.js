03aab35833d15a83b0066fbb0bac4bdfffc37167
Lassi Inkinen <lassi.inkinen@tuni.fi>
2023-11-25 21:02:16 +0200

    add: content container

---
+++ tip-calc.js
@@ -0,0 +1,9 @@
+(function () {
+  const element = `
+  <div class="container">
+    <h1>Tip Calculator</h1>
+  </div>`;
+
+  content = document.getElementById("content"); // eslint-disable-line
+  content.innerHTML = element; // eslint-disable-line
+})();
