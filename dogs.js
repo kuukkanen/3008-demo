e5e9234e6b2d86a737c57df0cef5790205701183
joonaskiuru <joonas.kiuru@gmail.com>
2023-11-26 01:37:17 +0200

    add: error handling

--- dogs.js
+++ dogs.js
@@ -17,17 +17,29 @@ button.onclick = () => {
 async function getDog() {
     const img = document.getElementById('Dog');
     const url = await getImageLink();
-    img.src = url;
+    const response = await fetch(url);
+    if (!response.ok){
+        img.src = "https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg?w=740&t=st=1700954913~exp=1700955513~hmac=519e5fb1ea1b02694cc828c13a67af56109c26323c2cc483d78d373a8149eda9";
+    }
+    else {
+        img.src = url;
+    }
 }
 
 async function getImageLink () {
-    const url = "https://dog.ceo/api/breeds/image/random";
-    const response = await fetch(url,{
-        headers: {
-            "Accept": "application/json",
-          }});
-    const result = await response.json();
-    return result.message
+    const img = document.getElementById('Dog');
+    try {
+        const url = "https://dog.ceo/api/breeds/image/random";
+        const response = await fetch(url,{
+            headers: {
+                "Accept": "application/json",
+              }});
+        const result = await response.json();
+        return result.message
+    } catch (error) {
+        img.src = "https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg?w=740&t=st=1700954913~exp=1700955513~hmac=519e5fb1ea1b02694cc828c13a67af56109c26323c2cc483d78d373a8149eda9";
+    }
+
 }
 
 
