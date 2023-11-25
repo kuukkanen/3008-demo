3bc4241a5ab8afdaec6712d3aaaa3d652b76ec42
Lassi Inkinen <lassi.inkinen@tuni.fi>
2023-11-25 21:23:46 +0200

    add: html forms

--- tip-calc.js
+++ tip-calc.js
@@ -2,6 +2,16 @@
   const element = `
   <div class="container">
     <h1>Tip Calculator</h1>
+    <div>
+      <input type="number" id="bill" placeholder="Total Bill" />
+      <input type="number" id="tip" placeholder="Tip" />
+      <button id="calculate"></button>
+    </div>
+    <div>
+      <h2>Results</h2>
+      <span>Total Bill with Tip</span>
+      <input type="number" id="totalWithTip" disabled />
+    </div>
   </div>`;
 
   content = document.getElementById("content"); // eslint-disable-line
