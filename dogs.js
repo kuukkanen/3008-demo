9f50cdd5bc66cbf7ac58b12a2f445533130ca072
joonaskiuru <joonas.kiuru@gmail.com>
2023-11-25 23:08:30 +0200

    add: image

--- dogs.js
+++ dogs.js
@@ -4,10 +4,14 @@
       // Header element with divider
   const element = `<div class="container">
                     <h1>Dog Image Generator</h1>
+                    <img id="Dog" src=https://dog.ceo/api/breeds/image/random">
                     <button onClick=getDog()> Get Dog Image</button>
                     </div>`;
 content.innerHTML = element; // eslint-disable-line
 // https://dog.ceo/api/breeds/image/random
 
+function getDog() {
+
+}
 
 })();
